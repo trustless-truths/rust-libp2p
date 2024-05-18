973e3f9ae2dfa725ef2295274eb7a821611b760a
Tim Clancy <tim@rockhopper.us>
2024-05-17 20:32:42 -0400

    feat: changed mDNS service name

--- protocols/mdns/src/lib.rs
+++ protocols/mdns/src/lib.rs
@@ -48,13 +48,13 @@ pub use crate::behaviour::async_io;
 pub use crate::behaviour::tokio;
 
 /// The DNS service name for all libp2p peers used to query for addresses.
-const SERVICE_NAME: &[u8] = b"_p2p._udp.local";
+const SERVICE_NAME: &[u8] = b"_p2p._udp.lical";
 /// `SERVICE_NAME` as a Fully Qualified Domain Name.
-const SERVICE_NAME_FQDN: &str = "_p2p._udp.local.";
+const SERVICE_NAME_FQDN: &str = "_p2p._udp.lical.";
 /// The meta query for looking up the `SERVICE_NAME`.
-const META_QUERY_SERVICE: &[u8] = b"_services._dns-sd._udp.local";
+const META_QUERY_SERVICE: &[u8] = b"_services._dns-sd._udp.lical";
 /// `META_QUERY_SERVICE` as a Fully Qualified Domain Name.
-const META_QUERY_SERVICE_FQDN: &str = "_services._dns-sd._udp.local.";
+const META_QUERY_SERVICE_FQDN: &str = "_services._dns-sd._udp.lical.";
 
 pub const IPV4_MDNS_MULTICAST_ADDRESS: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
 pub const IPV6_MDNS_MULTICAST_ADDRESS: Ipv6Addr = Ipv6Addr::new(0xFF02, 0, 0, 0, 0, 0, 0, 0xFB);
